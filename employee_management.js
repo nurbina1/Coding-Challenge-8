8a0277f13734bea53118dbe58c36a01a8680722d
Abylkhair03 <nurbina@usf.edu>
2024-10-06 23:22:13 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -12,3 +12,23 @@ class Employee {
         return `${this.name} works as a ${this.position} in ${this.department}, earning $${this.salary}.`;
     }
 }
+
+// Represents a department, managing a list of employees and calculating total salaries.
+class Department {
+    constructor(name) {
+        this.name = name;
+        this.employees = []; // Stores the employees belonging to the department
+    }
+
+    // Adds an employee to the department's employee list.
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+
+    // Calculates the total salary of all employees in the department.
+    getDepartmentSalary() {
+        return this.employees.reduce((total, employee) => total + employee.salary, 0);
+    }
+
+
+}
